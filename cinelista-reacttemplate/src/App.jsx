708d4f96fd986641594ff416cf52e364852bdf86
Deibi Andrés Hernández cambar <deibi.hernandez@inacapmail.cl>
2026-06-22 09:57:00 -0400

    etapa 3
    renderizado de la lista con key

--- cinelista-reacttemplate/src/App.jsx
+++ cinelista-reacttemplate/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { peliculas } from "./data/peliculas";
+import ListaPeliculas from "./components/ListaPeliculas";
 
 function App() {
     console.log("Movies array:", peliculas);
@@ -7,6 +8,7 @@ function App() {
     return (
     <div>
         <h1>Welcome to CineLista</h1>
+        <ListaPeliculas peliculas={peliculas} />
     </div>
     );
 }
