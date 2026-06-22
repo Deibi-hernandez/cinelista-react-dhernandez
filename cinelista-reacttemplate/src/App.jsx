b51fc72d52785a6fd867a5e2c7e8309df1e39ebf
Deibi Andrés Hernández cambar <deibi.hernandez@inacapmail.cl>
2026-06-22 09:23:19 -0400

    primer commint
    proyecto base con datos

---
+++ cinelista-reacttemplate/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { peliculas } from "./data/peliculas";
+
+function App() {
+    console.log("Movies array:", peliculas);
+
+    return (
+    <div>
+        <h1>Welcome to CineLista</h1>
+    </div>
+    );
+}
+
+export default App;
